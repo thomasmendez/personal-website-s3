7f263afcb19f7995ea549086c02bde1bd1850e31
Thomas Antonio Mendez <thomasmendez01@gmail.com>
2021-08-27 02:52:14 -0500

    Hardcoded change

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,8 +5,8 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "mybucket"
-    key    = "mybucketkey"
+    bucket = "mybucketdev"
+    key    = "mybucketdevkey"
     region = "us-east-2"
   }
 }
