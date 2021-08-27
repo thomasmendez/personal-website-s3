db72d70a938b04122cd71872e05736163946d8e2
Thomas Antonio Mendez <thomasmendez01@gmail.com>
2021-08-27 01:34:30 -0500

    Update variables.tf to have default placeholders

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,15 @@
 variable "env" {
     description = "Environment"
     type = string
+    default = "dev"
 }
 variable "aws_region" {
     description = "AWS region"
     type = string
+    default = "us-east-2"
 }
 variable "aws_bucket_name" {
     description = "S3 Bucket Name"
     type = string
+    default = "bucketname"
 }
