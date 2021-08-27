a020ac3bb971807cfb7783213a2923763f494a05
Thomas Antonio Mendez <thomasmendez01@gmail.com>
2021-08-27 02:17:36 -0500

    Updated to configure remote state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,3 +2,11 @@ provider "aws" {
     version = "~> 2.0"
     region  = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket = var.aws_bucket_name
+    key    = "${var.aws_bucket_name}key"
+    region = "eu-central-1"
+  }
+}
