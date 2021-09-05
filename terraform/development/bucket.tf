86d4613e7419798f7747b7c0d9564b05d0b4802f
Thomas Antonio Mendez <thomasmendez01@gmail.com>
2021-09-05 03:27:05 -0500

    Added lifecyle rule

--- terraform/development/bucket.tf
+++ terraform/development/bucket.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "demos3" {
   tags = {
     Environment = var.env
   }
-  lifecycle_rule = {
+  lifecycle_rule {
     id      = "cleanup"
     prefix  = "cleanup/"
     enabled = true
