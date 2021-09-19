bd5a79473f5f4f3084765cb893073d53925e14fc
Thomas Antonio Mendez <thomasmendez01@gmail.com>
2021-09-18 19:17:09 -0500

    Webpack files now check for linting errors

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -19,6 +20,14 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'public', 'index.html'),
+    }),
+    new ESLintPlugin({
+      extensions: ['.js'],
+      exclude: 'node_modules',
+      emitError: false,
+      failOnError: false,
+      emitWarning: false,
+      failOnWarning: false,
     }),
   ],
 };
