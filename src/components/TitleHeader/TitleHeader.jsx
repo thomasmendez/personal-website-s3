7215afbedd3453d36dd019a530613a204015b4ea
Thomas Antonio Mendez <thomasmendez01@gmail.com>
2021-12-19 14:57:41 -0600

    Refactored into view

--- src/components/TitleHeader/TitleHeader.jsx
+++ src/components/TitleHeader/TitleHeader.jsx
@@ -4,12 +4,6 @@ import { Grid, Box, Typography } from '@mui/material';
 
 import ProfilePic from '../../assets/pic.jpeg';
 
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import EmailIcon from '@mui/icons-material/Email';
-
-import ContactMethod from '../ContactMethod';
-
 const useStyles = makeStyles({
   centerItems: {
     alignItems: 'center',
@@ -25,17 +19,6 @@ const useStyles = makeStyles({
   lightText: {
     color: '#aaa',
   },
-  bold: {
-    fontWeight: 600,
-  },
-  noTextDecoration: {
-    textDecoration: 'none',
-    color: 'black',
-    //hover: false,
-    '&:hover': {
-      color: '#800080',
-    },
-  },
 });
 
 const TitleHeader = () => {
@@ -43,86 +26,31 @@ const TitleHeader = () => {
   return(
     <Grid
       container
+      item
+      className={classes.lightBackground}
+      alignItems="center"
+      justifyContent="center"
+      textAlign="center"
       spacing={3}
+      pb={5}
     >
-      <Grid
-        container
-        item
-        className={classes.lightBackground}
-        alignItems="center"
-        justifyContent="center"
-        textAlign="center"
-        spacing={3}
-        pb={5}
-      >
-        <Grid item xs={12}>
-          <Box borderRadius="50%">
-            <img className={classes.roundedImage} src={ProfilePic} alt="100x100" />
-          </Box>
-        </Grid>
-        <Grid item xs={12}>
-          <Grid item>
-            <Typography variant="h4" display="block">
-              Thomas A Mendez
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography className={classes.lightText} variant="h5">
-              Software Engineer
-              <br />
-              and Game Developer
-            </Typography>
-          </Grid>
-        </Grid>
+      <Grid item xs={12}>
+        <Box borderRadius="50%">
+          <img className={classes.roundedImage} src={ProfilePic} alt="100x100" />
+        </Box>
       </Grid>
-      <Grid container item spacing={8}>
-        <Grid item sm={3} />
-        <Grid container item sm={4} spacing={3}>
-          <Grid item>
-            <Typography variant="h5">
-              About Me
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography>
-              I am a passionate software engineer who loves to solve real world problems using new unexplored technologies.
-              I enjoy developing good user experiences, wether it be for desktop, mobile, or video games.
-              Always looking forward to working on the next big project.
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography>
-              EDUCATION
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography>
-              Bachelor of Science in Arts and Entertainment Technologies, at The University of Texas at Austin
-              August 2016 - May 2018
-            </Typography>
-          </Grid>
+      <Grid item xs={12}>
+        <Grid item>
+          <Typography variant="h4" display="block">
+            Thomas A Mendez
+          </Typography>
         </Grid>
-        <Grid container item sm={3}>
-          <Grid item xs={12}>
-            <Typography variant="h5">
-              Contact
-            </Typography>
-          </Grid>
-          <ContactMethod
-            contactMethodName='LinkedIn'
-            icon={<LinkedInIcon />}
-            hrefLink='https://www.linkedin.com/in/thomas-a-mendez'
-          />
-          <ContactMethod
-            contactMethodName='Github'
-            icon={<GitHubIcon />}
-            hrefLink='https://github.com/thomasmendez'
-          />
-          <ContactMethod
-            contactMethodName='Email'
-            icon={<EmailIcon />}
-            hrefLink='mailto:thomasmendez01@gmail.com'
-          />
+        <Grid item>
+          <Typography className={classes.lightText} variant="h5">
+            Software Engineer
+            <br />
+            and Game Developer
+          </Typography>
         </Grid>
       </Grid>
     </Grid>
--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,8 +1,69 @@
 import React from 'react';
+import { Grid, Typography } from '@mui/material';
 import TitleHeader from '../components/TitleHeader';
+import ContactMethod from '../components/ContactMethod';
+
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
 
 const About = () => (
-  <TitleHeader />
+  <Grid
+    container
+    spacing={3}
+  >
+    <TitleHeader />
+    <Grid container item spacing={8}>
+      <Grid item sm={3} />
+      <Grid container item sm={4} spacing={3}>
+        <Grid item>
+          <Typography variant="h5">
+            About Me
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography>
+            I am a passionate software engineer who loves to solve real world problems using new unexplored technologies.
+            I enjoy developing good user experiences, wether it be for desktop, mobile, or video games.
+            Always looking forward to working on the next big project.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography>
+            EDUCATION
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography>
+            Bachelor of Science in Arts and Entertainment Technologies, at The University of Texas at Austin
+            August 2016 - May 2018
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container item sm={3}>
+        <Grid item xs={12}>
+          <Typography variant="h5">
+            Contact
+          </Typography>
+        </Grid>
+        <ContactMethod
+          contactMethodName='LinkedIn'
+          icon={<LinkedInIcon />}
+          hrefLink='https://www.linkedin.com/in/thomas-a-mendez'
+        />
+        <ContactMethod
+          contactMethodName='Github'
+          icon={<GitHubIcon />}
+          hrefLink='https://github.com/thomasmendez'
+        />
+        <ContactMethod
+          contactMethodName='Email'
+          icon={<EmailIcon />}
+          hrefLink='mailto:thomasmendez01@gmail.com'
+        />
+      </Grid>
+    </Grid>
+  </Grid>
 );
 
 export default About;
