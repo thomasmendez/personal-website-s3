e6bf68a0f7abb720ff0ac1c7f8a2e59fb5af286f
Thomas Mendez <thomasmendez01@gmail.com>
2021-12-23 20:39:41 -0600

    Removed commment

--- src/components/About/Contact/Contact.jsx
+++ src/components/About/Contact/Contact.jsx
@@ -38,31 +38,4 @@ Contact.propTypes = {
   ),
 };
 
-// const Contact = (props) => {
-//     return(
-//       <Grid container item sm={3}>
-//         <Grid item xs={12}>
-//           <Typography variant="h5">
-//             Contact
-//           </Typography>
-//         </Grid>       
-//         <ContactMethod
-//           contactMethodName='LinkedIn'
-//           icon={<LinkedInIcon />}
-//           hrefLink='https://www.linkedin.com/in/thomas-a-mendez'
-//         />
-//         <ContactMethod
-//           contactMethodName='Github'
-//           icon={<GitHubIcon />}
-//           hrefLink='https://github.com/thomasmendez'
-//         />
-//         <ContactMethod
-//           contactMethodName='Email'
-//           icon={<EmailIcon />}
-//           hrefLink='mailto:thomasmendez01@gmail.com'
-//         />
-//       </Grid>
-//     );
-// };
-
 export default Contact;
