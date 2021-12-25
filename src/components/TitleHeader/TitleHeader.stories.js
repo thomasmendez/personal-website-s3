e684e5529c5ca55418e891017dfa61841eb88b73
Thomas Mendez <thomasmendez01@gmail.com>
2021-12-24 18:38:28 -0600

    Moved TitleHeader.story.js

---
+++ src/components/TitleHeader/TitleHeader.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TitleHeader from './TitleHeader';
+
+export default {
+  title: 'Components/TitleHeader',
+  component: TitleHeader,
+  argTypes: {
+    backgroundColor: { control: 'color' }, // Color controls will be detected automatically
+    label: { control: 'text' }, // Text controls may be added as extra
+    text: { control: 'text' }, // Text controls may be added as extra
+  },
+  parameters: {
+    applyColorTo: ['label'], // Must match argType key
+  },
+};
+
+const Template = args => <TitleHeader {...args}/>;
+
+export const Navigation = Template.bind({});
