2267b556606173e3cda00e608587ea0ac70f7978
Thomas Antonio Mendez <thomasmendez01@gmail.com>
2022-01-02 15:19:55 -0600

    Fixed layout issue for mobile for about me section

--- src/components/About/AboutMe/AboutMe.jsx
+++ src/components/About/AboutMe/AboutMe.jsx
@@ -4,29 +4,57 @@ import { Grid, Typography } from '@mui/material';
 const AboutMe = () => {
     return(
       <Grid container item sm={6} md={4} spacing={3}>
-        <Grid item>
+        <Grid item xs={1}/>
+        <Grid item xs={10}>
           <Typography variant="h5">
             About Me
           </Typography>
         </Grid>
-        <Grid item>
+        <Grid item xs={1}/>
+
+        <Grid item xs={1}/>
+        <Grid item xs={10}>
           <Typography>
             I am a passionate software engineer who loves to solve real world problems using new unexplored technologies.
             I enjoy developing good user experiences, wether it be for desktop, mobile, or video games.
             Always looking forward to working on the next big project.
           </Typography>
         </Grid>
-        <Grid item>
-          <Typography>
-            EDUCATION
+        <Grid item xs={1}/>
+
+        <Grid item xs={1}/>
+        <Grid item xs={10}>
+          <Typography variant="h6">
+            Education
           </Typography>
         </Grid>
-        <Grid item>
+        <Grid item xs={1}/>
+
+        <Grid item xs={1}/>
+        <Grid item xs={10}>
           <Typography>
             Bachelor of Science in Arts and Entertainment Technologies, at The University of Texas at Austin
-            August 2016 - May 2018
           </Typography>
         </Grid>
+        <Grid item xs={1}/>
+
+        <Grid item xs={1}/>
+        <Grid item xs={10}>
+          August 2016 - May 2019
+        </Grid>
+        <Grid item xs={1}/>
+
+        <Grid item xs={1}/>
+        <Grid item xs={10}>
+          <ul>
+            <li>
+              <Typography>
+                Elements of Computing Certificate
+              </Typography>
+            </li>
+          </ul>
+        </Grid>
+        <Grid item xs={1}/>
       </Grid>
     );
 };
