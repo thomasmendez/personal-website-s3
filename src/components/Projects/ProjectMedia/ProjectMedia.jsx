f01096d2cca76ac068a69e266caab59774c822c0
Thomas Antonio Mendez <thomasmendez01@gmail.com>
2022-01-02 21:28:57 -0600

    Added missing card element

--- src/components/Projects/ProjectMedia/ProjectMedia.jsx
+++ src/components/Projects/ProjectMedia/ProjectMedia.jsx
@@ -1,50 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button } from '@mui/material';
+import { Grid, Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button } from '@mui/material';
 
 const ProjectMedia = (props) => {
   const { media, mediaAlt, mediaTitle, mediaDescription, website, repository, youTubeLink, downloadLink } = props;
   const extension = media.split('.').pop();
   return(
     <Grid container item>
-      <CardActionArea>
-        <CardMedia
-          component={extension === 'mp4' ? 'video' : 'img'}
-          image={media}
-          alt={mediaAlt}
-          controls={extension === 'mp4'}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {mediaTitle}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {mediaDescription}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        {website && (
-          <Button size="small" color="primary" href={website} target="_blank">
-            Website
-          </Button>
-        )}
-        {repository && (
-          <Button size="small" color="primary" href={repository} target="_blank">
-            Repository
-          </Button>
-        )}
-        {youTubeLink && (
-          <Button size="small" color="primary" href={website} target="_blank">
-            YouTube
-          </Button>
-        )}
-        {downloadLink && (
-          <Button size="small" color="primary" href={repository} target="_blank">
-            Download
-          </Button>
-        )}
-      </CardActions>
+      <Card>
+        <CardActionArea>
+          <CardMedia
+            component={extension === 'mp4' ? 'video' : 'img'}
+            image={media}
+            alt={mediaAlt}
+            controls={extension === 'mp4'}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {mediaTitle}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {mediaDescription}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          {website && (
+            <Button size="small" color="primary" href={website} target="_blank">
+              Website
+            </Button>
+          )}
+          {repository && (
+            <Button size="small" color="primary" href={repository} target="_blank">
+              Repository
+            </Button>
+          )}
+          {youTubeLink && (
+            <Button size="small" color="primary" href={website} target="_blank">
+              YouTube
+            </Button>
+          )}
+          {downloadLink && (
+            <Button size="small" color="primary" href={repository} target="_blank">
+              Download
+            </Button>
+          )}
+        </CardActions>
+      </Card>
     </Grid>
   );
 };
