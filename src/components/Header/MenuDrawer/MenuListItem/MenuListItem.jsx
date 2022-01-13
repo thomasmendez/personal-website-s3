aaa7d7dce220c55084d6dc8ce72ea4e94e83fccb
Thomas Antonio Mendez <thomasmendez01@gmail.com>
2022-01-12 21:27:31 -0600

    Removed prop requirement

--- src/components/Header/MenuDrawer/MenuListItem/MenuListItem.jsx
+++ src/components/Header/MenuDrawer/MenuListItem/MenuListItem.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ListItem, ListItemText } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { requireAtLeastOne } from '../../../../utils/propTypesUtils';
 
 const MenuListItem = (props) => {
   const { to, name, setOpen, link } = props;
@@ -20,16 +19,11 @@ const MenuListItem = (props) => {
   );
 };
 
-const requireOne = requireAtLeastOne({
-  to: PropTypes.string,
-  link: PropTypes.string,
-});
-
 MenuListItem.propTypes = {
-  to: requireOne,
+  to: PropTypes.string,
   name: PropTypes.string.isRequired,
   setOpen: PropTypes.func.isRequired,
-  link: requireOne,
+  link: PropTypes.string,
 };
 
 export default MenuListItem;
--- src/components/Header/RouteButton/RouteButton.jsx
+++ src/components/Header/RouteButton/RouteButton.jsx
@@ -18,34 +18,10 @@ const RouteButton = ({ to, children, link }) => {
   );
 };
 
-function requireAtLeastOne(checkProps) {
-  return function(props, propName, compName) {
-    const requirePropNames = Object.keys(checkProps);
-    const found = requirePropNames.find((propRequired) => props[propRequired]);
-    try {
-      if (!found) {
-        throw new Error(
-          `One of ${requirePropNames.join(',')} is required by '${compName}' component.`,
-        );
-      }
-      PropTypes.checkPropTypes(checkProps, props, propName, compName);
-    } catch (e) {
-      return e;
-    }
-    return null;
-  };
-}
-
-
-const requireOne = requireAtLeastOne({
-  to: PropTypes.string,
-  link: PropTypes.string,
-});
-
 RouteButton.propTypes = {
-  to: requireOne,
+  to: PropTypes.string,
   children: PropTypes.string.isRequired,
-  link: requireOne,
+  link: PropTypes.string,
 };
 
 export default RouteButton;
