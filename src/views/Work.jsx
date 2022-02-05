71b0c72e7f0678fab0a3d913f8b8e7e96c3021f6
Thomas Antonio Mendez <thomasmendez01@gmail.com>
2022-02-04 21:30:08 -0600

    Updated website and resume content

--- src/views/Work.jsx
+++ src/views/Work.jsx
@@ -18,7 +18,8 @@ const EXPERIENCE = [
     jobRole: 'Software Engineer',
     jobDescription: [
       'Works with team through full software development life cycle in order to deliver quarterly features and deliverables based on customer needs',
-      'Focused on delivering front-end research, design, and features for internal team product',
+      'Focuses on delivering front-end research, design, and features for internal team product',
+      'Works in the development of back-end GraphQL server features for internal team product',
       'Created Python package intended to report non-inclusive words present in a directory and suggest alternative words',
     ],
   },
